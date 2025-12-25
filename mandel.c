9c38d92ba3809babb8d66fba53618ed065bacd4f
mcleod_ideafix <miguel.angel@zxprojects.com>
2025-12-25 04:48:32 +0100

    modified:   mandel.c mandel.prg pixel.prg sysinfo.prg

--- mandel.c
+++ mandel.c
@@ -16,8 +16,8 @@
 #define BPS8     0x03
 #define BPS16    0x04    /* 16 bpp truecolor */
 
-#define WIDTH    320
-#define HEIGHT   200
+#define WIDTH    640
+#define HEIGHT   400
 
 /* LUT de 256 entradas, en formato RGB 5-6-5 */
 static uint16_t mcga_lut[256];
@@ -27,9 +27,9 @@ uint16_t *screen = NULL;
 /* Convierte componentes MCGA de 0..63 a 16 bits 5-6-5 */
 static uint16_t rgb6_to_565(uint8_t r6, uint8_t g6, uint8_t b6)
 {
-    uint8_t r5 = (uint8_t)(r6 >> 1);     /* 6->5 bits */
-    uint8_t g6c = g6;                    /* 6 bits ya */
-    uint8_t b5 = (uint8_t)(b6 >> 1);
+    uint8_t r5  = (uint8_t)(r6 >> 1);   /* 6->5 bits */
+    uint8_t g6c = g6;                   /* 6 bits ya */
+    uint8_t b5  = (uint8_t)(b6 >> 1);
 
     return (uint16_t)((r5 << 11) | (g6c << 5) | b5);
 }
@@ -86,116 +86,146 @@ static void init_mcga_lut(void)
 /* Escribe un pixel “chunky” 16 bpp usando índice MCGA (0..255) */
 static void plot (uint16_t x, uint16_t y, uint8_t color_index)
 {
-  uint32_t offset = (uint32_t)y * WIDTH + (uint32_t)x;
-  screen[offset] = mcga_lut[color_index];
+    screen[(uint32_t)y * WIDTH + (uint32_t)x] = mcga_lut[color_index];
 }
 
 static uint16_t point565 (uint16_t x, uint16_t y)
 {
-  uint32_t offset = (uint32_t)y * WIDTH + (uint32_t)x;
-  return screen[offset];
+    return screen[(uint32_t)y * WIDTH + (uint32_t)x];
 }
 
 static void plot565 (uint16_t x, uint16_t y, uint16_t c)
 {
-  uint32_t offset = (uint32_t)y * WIDTH + (uint32_t)x;
-  screen[offset] = c;
-}  
+    screen[(uint32_t)y * WIDTH + (uint32_t)x] = c;
+}
 
-static uint16_t tiempo_escape (uint16_t x, uint16_t y)
+#define tiempo_escape tiempo_escape_fx
+//#define tiempo_escape tiempo_escape_fp
+static uint16_t tiempo_escape_fp (uint16_t x, uint16_t y) 
 {
-  float xmin = -2.4;
-  float xmax = 0.8;
-  float ymin = -1.2;
-  float ymax = 1.2;
-  float cr, ci, zr, zi, tr, ti;
-  uint8_t color;
-
-  cr = xmin + x * (xmax - xmin) / (WIDTH - 1);
-  ci = ymin + y * (ymax - ymin) / (HEIGHT - 1);
-  zr = cr;
-  zi = ci;
+  float xmin = -2.4; 
+  float xmax = 0.8; 
+  float ymin = -1.2; 
+  float ymax = 1.2; 
+  float cr, ci, zr, zi, tr, ti; 
+  uint8_t color; 
   
-  color = 0;
-  while (zr*zr+zi*zi<4 && color != 255)
+  cr = xmin + x * (xmax - xmin) / (WIDTH - 1); 
+  ci = ymin + y * (ymax - ymin) / (HEIGHT - 1); 
+  zr = cr; 
+  zi = ci; 
+  color = 0; 
+  
+  while (zr*zr+zi*zi<4 && color != 255) 
+  { 
+    color++; 
+    tr = zr*zr - zi*zi + cr; 
+    ti = 2.0*zr*zi + ci; 
+    zr = tr; 
+    zi = ti; 
+  } 
+  return mcga_lut[color]; 
+}
+
+static uint16_t tiempo_escape_fx (uint16_t x, uint16_t y)
+{
+  int64_t cr = -157286 + x*209715/(WIDTH-1);  // trabajamos con Q(16.16)
+  int64_t ci = -78643 + y*157286/(HEIGHT-1);
+  int64_t zr = cr;
+  int64_t zi = ci;
+
+  uint8_t cont = 0;
+  while (cont != 255 && zr*zr+zi*zi<17179869184LL)
   {
-    color++;
-    tr = zr*zr - zi*zi + cr;
-    ti = 2.0*zr*zi + ci;
+    cont++;
+    int64_t tr = (zr*zr-zi*zi)/65536 + cr;
+    int64_t ti = (2*zr*zi)/65536 + ci;
     zr = tr;
     zi = ti;
   }
-  return mcga_lut[color];
+  return mcga_lut[cont];
 }
 
 static void mandel_r (uint16_t xm, uint16_t ym, uint16_t w, uint16_t h)
 {
-  if (w <= 8 || h <= 8)
-  {
-    for (int y=ym; y<ym+h; y++)
-    {
-      for (int x=xm; x<xm+w; x++)
-      {
-        plot565 (x, y, tiempo_escape (x, y));
-      }
+    if (w <= 4 || h <= 4) {
+        uint16_t x, y;
+        for (y = ym; y < ym + h; y++) {
+            for (x = xm; x < xm + w; x++) {
+                plot565 (x, y, tiempo_escape (x, y));
+            }
+        }
+        return;
     }
-    return;
-  }
-  
-  uint32_t cref = point565 (xm, ym);
 
-  for (int x=xm; x<xm+w; x++)
-  {
-    uint16_t c = tiempo_escape (x, ym);
-    plot565 (x, ym, c);
-    if (cref != c)
-      goto particionar;
-    c = tiempo_escape (x, ym+h-1);
-    plot565 (x, ym+h-1, c);
-    if (cref != c)
-      goto particionar;
-  }
+    {
+        uint16_t x, y;
+        uint16_t cref = tiempo_escape (xm, ym);
 
-  for (int y=ym; y<ym+h; y++)
-  {
-    uint16_t c = tiempo_escape (xm, y);
-    plot565 (xm, y, c);
-    if (cref != c)
-      goto particionar;
-    c = tiempo_escape (xm+w-1, y);
-    plot565 (xm+w-1, y, c);
-    if (cref != c)
-      goto particionar;
-  }
-  
-  for (int y=ym+1; y<ym+h-1; y++)
-    for (int x=xm+1; x<xm+w-1; x++)
-      plot565 (x, y, cref);
-  return;
-        
+        /* Pintamos bordes y comprobamos homogeneidad */
+        for (x = xm; x < xm + w; x++) {
+            uint16_t c;
+
+            c = tiempo_escape (x, ym);
+            plot565 (x, ym, c);
+            if (cref != c)
+                goto particionar;
+
+            c = tiempo_escape (x, ym + h - 1);
+            plot565 (x, ym + h - 1, c);
+            if (cref != c)
+                goto particionar;
+        }
+
+        for (y = ym; y < ym + h; y++) {
+            uint16_t c;
+
+            c = tiempo_escape (xm, y);
+            plot565 (xm, y, c);
+            if (cref != c)
+                goto particionar;
+
+            c = tiempo_escape (xm + w - 1, y);
+            plot565 (xm + w - 1, y, c);
+            if (cref != c)
+                goto particionar;
+        }
+
+        /* Rellenar interior con el color homogéneo */
+        for (y = ym + 1; y < ym + h - 1; y++) {
+            uint16_t x2;
+            for (x2 = xm + 1; x2 < xm + w - 1; x2++) {
+                plot565 (x2, y, cref);
+            }
+        }
+        return;
+
 particionar:
-  mandel_r (xm, ym, w/2, h/2);
-  mandel_r (xm + w/2, ym, w-w/2, h/2);
-  mandel_r (xm, ym + h/2, w/2, h-h/2);
-  mandel_r (xm + w/2, ym + h/2, w-w/2, h-h/2);
+        {
+            uint16_t w2 = w / 2;
+            uint16_t h2 = h / 2;
+            mandel_r (xm,          ym,          w2,     h2);
+            mandel_r (xm + w2,     ym,          w - w2, h2);
+            mandel_r (xm,          ym + h2,     w2,     h - h2);
+            mandel_r (xm + w2,     ym + h2,     w - w2, h - h2);
+        }
+    }
 }
 
 static void mandel_recursivo (void)
 {
-  mandel_r (0, 0, WIDTH, HEIGHT);
+    mandel_r (0, 0, WIDTH, HEIGHT);
 }
 
 static void mandel (void)
 {
-  uint16_t x, y;
-  
-  for (y=0; y<HEIGHT; y++)
-  {
-    for (x=0; x<WIDTH; x++)
-    {
-      plot565 (x, y, tiempo_escape (x, y));
+    uint16_t x, y;
+
+    for (y = 0; y < HEIGHT; y++) {
+        for (x = 0; x < WIDTH; x++) {
+            plot565 (x, y, tiempo_escape (x, y));
+        }
     }
-  }
 }
 
 /* Programa principal */
@@ -207,12 +237,11 @@ int main(void)
     void    *old_log;
     void    *old_phys;
 
-    /* Nuevo modo 320x200x16bpp chunky */
+    /* Nuevo modo 640x400x16bpp chunky */
     int16_t  monitor_bits;
     int16_t  freq_bits;
     int16_t  new_mode;
     long     new_size;
-    long     i;
 
     old_mode = VsetMode(-1);      /* solo consultar */
     old_rez  = Getrez();
@@ -223,13 +252,14 @@ int main(void)
     monitor_bits = (int16_t)(old_mode & VGA);
     freq_bits    = (int16_t)(old_mode & PAL);
 
-    /* Modo Falcon: 320x200 truecolor, 40 columnas, sin ST compat */
+    /* Modo Falcon: 640x400 truecolor, 80 columnas, sin ST compat */
     new_mode = (int16_t)(
         (old_mode & ~(STMODES | BPS1 | BPS2 | BPS4 | BPS8)) |
-        COL40 |
+        COL80 |
         BPS16 |
-        monitor_bits |
-        freq_bits
+        VERTFLAG |    /* interlaced 400 líneas */
+        freq_bits      /* normalmente PAL */
+        /* monitor_bits será 0 (TV) si no estabas en VGA */
     );
 
     /* Tamaño necesario para el nuevo modo */
@@ -252,12 +282,14 @@ int main(void)
     /* Cambiar modo de vídeo */
     VsetMode(new_mode);
 
-    /* Inicializar LUT MCGA -> RGB565 */
+    /* Inicializar LUT MCGA -> RGB565 y parámetros fijos del fractal */
     init_mcga_lut();
 
-    memset (screen, 0, new_size);
+    memset(screen, 0, new_size);
 
-    mandel_recursivo(); 
+    /* Elegir una de las dos versiones */
+    mandel_recursivo();
+    /* o bien: mandel(); */
 
     Cconin();
 
